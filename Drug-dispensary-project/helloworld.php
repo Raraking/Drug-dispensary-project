7f0605647ef6ea9847c06d6f5746d49846968f1e
Raraking <111497328+Raraking@users.noreply.github.com>
2023-04-25 10:29:25 +0300

    Add files via upload

---
+++ Drug-dispensary-project/helloworld.php
@@ -0,0 +1,14 @@
+<?php
+function addition ($num1, $num2)
+{
+    $sum = $num1 + $num2;
+    return $sum;
+}
+$num1 = 3; $num2 = 2;
+echo "Hello world this is the sum of $num1 and $num2", "<br>";
+    $sum = addition($num1,$num2);
+echo $sum;
+    
+?>
+
+
