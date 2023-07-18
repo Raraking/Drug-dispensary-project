46a5731ec8500dd2822eef77426ad7ba625fb4f8
nlcol3gon3 <131747232+nlcol3gon3@users.noreply.github.com>
2023-07-18 16:09:42 +0300

    database.php

--- pharmaflow/database.php
+++ pharmaflow/database.php
@@ -1,5 +1,4 @@
 <?php
-//echo session_save_path();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -8,10 +7,8 @@ $username = "root";
 $password = "";
 $dbname = "pharmaflow";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
